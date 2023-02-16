4b306403acad755966d69ad67ecaa654ee96c1b3
RyjkovAlexey <kawai4ik98@gmail.com>
2023-02-16 16:05:03 +0300

    scroll by anchor and menu items active state

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,44 @@
-const init = () => {
+const toggleClass = (hash, className = 'page-menu__link--active') => {
+	if (hash) {
+		const link = document.querySelector(`a[href="${hash}"]`);
+
+		link && link.classList.toggle(className);
+	}
+};
+
+const toggleMobileMenu = force => {
 	const burger = document.querySelector('.burger');
 	const menu = document.querySelector('.page-menu');
 
-	burger.addEventListener('click', () => {
-		burger.classList.toggle('burger--opened');
-		menu.classList.toggle('page-menu--opened');
-		document.body.classList.toggle('body--menu-opened');
+	burger.classList.toggle('burger--opened', force);
+	menu.classList.toggle('page-menu--opened', force);
+	document.body.classList.toggle('body--menu-opened', force);
+};
+
+const init = () => {
+	const burger = document.querySelector('.burger');
+
+	burger.addEventListener('click', () => toggleMobileMenu());
+
+	addEventListener('hashchange', event => {
+		const oldHash = new URL(event.oldURL).hash;
+		const newHash = new URL(event.newURL).hash;
+
+		toggleClass(oldHash);
+
+		toggleClass(newHash);
+
+		toggleMobileMenu(false);
 	});
 
-	console.log(burger);
+	if (location?.hash) {
+		toggleClass(location.hash);
+	} else {
+		const BASEHASH = '#main';
+
+		history.pushState(null, null, BASEHASH);
+		toggleClass(BASEHASH);
+	}
 };
 
 document.addEventListener('DOMContentLoaded', init);
